bc367c3db677cd4989375745519a398a601cf668
JJFA-777 <jesunboalade@gmail.com>
2025-04-19 02:57:49 +0000

    use of typeof to compare 'undefined' string

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,5 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
   return initialNumber
-   + (expansion1989 === undefined ? 89 : expansion1989)
-   + (expansion2019 === undefined ? 19 : expansion2019);
+   + (typeof expansion1989 === 'undefined' ? 89 : expansion1989)
+   + (typeof expansion2019 === 'undefined' ? 19 : expansion2019);
 }
