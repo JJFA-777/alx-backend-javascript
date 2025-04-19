bdae5b033025a19a885f5c27dc2cc5b74b0da801
JJFA-777 <jesunboalade@gmail.com>
2025-04-19 03:00:59 +0000

    use of void 0 instead of undefined

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,5 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
   return initialNumber
-   + (typeof expansion1989 === 'undefined' ? 89 : expansion1989)
-   + (typeof expansion2019 === 'undefined' ? 19 : expansion2019);
+   + (expansion1989 === void 0 ? 89 : expansion1989)
+   + (expansion2019 === void 0 ? 19 : expansion2019);
 }
